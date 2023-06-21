51c71e4c8746ae3d83092ecc675ebb3d3664eacc
Basit46 <Hassanbasit1990@gmail.com>
2023-06-21 19:18:38 +0100

    Added changes to menu bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,30 +41,36 @@ const Navbar = ({ openMenu, setOpenMenu }) => {
             <FaBars onClick={() => setOpenMenu(true)} className="text-[30px]" />
           )}
         </div>
-        <ul
+        <div
           className={`${
-            openMenu ? "h-[20vh] pb-[30px]" : "h-0 "
-          } duration-500 overflow-hidden px-[40px] flex flex-col gap-[20px] justify-between items-center`}
+            openMenu ? "h-[25vh] pb-[30px]" : "h-0 "
+          } duration-500 overflow-hidden px-[40px]`}
         >
-          <li className="hover:text-[#0088ff] cursor-pointer font-[500]">
-            Features
-          </li>
-          <li className="hover:text-[#0088ff] cursor-pointer font-[500]">
-            Roadmap
-          </li>
-          <a
-            href="https://angry-pyramid-987.notion.site/Documentation-Coming-Soon-3bfb301b92af4c0dba107bbaf80d2c83"
-            className="w-full h-[40px] grid place-items-center font-[800] text-[#222222] bg-white rounded-[8px]"
+          <ul
+            className={`${
+              openMenu ? "flex" : "hidden"
+            }  h-full w-full flex-col gap-[20px] items-center`}
           >
-            DOCS
-          </a>
-        </ul>
+            <li className="hover:text-[#0088ff] cursor-pointer font-[500]">
+              Features
+            </li>
+            <li className="hover:text-[#0088ff] cursor-pointer font-[500]">
+              Roadmap
+            </li>
+            <a
+              href="https://angry-pyramid-987.notion.site/Documentation-Coming-Soon-3bfb301b92af4c0dba107bbaf80d2c83"
+              className="w-full h-[50px] grid place-items-center font-[800] text-[#222222] bg-white rounded-[8px]"
+            >
+              DOCS
+            </a>
+          </ul>
+        </div>
       </nav>
 
       {/* Mobile Background */}
       <div
         className={`${
-          openMenu ? "h-[33.5vh]" : "h-0 hidden"
+          openMenu ? "h-[38vh]" : "h-0 hidden"
         } md:hidden z-[0] fixed top-0 w-full h-[35vh] bg-[#0c0150]`}
       />
     </>
